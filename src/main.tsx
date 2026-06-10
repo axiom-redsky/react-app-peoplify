b233be24efb9dfec037c5b14ef9b1560d6dd670c
redsky0212 <wihyun0238@gmail.com>
2026-06-10 17:13:46 +0900

    api base url수정

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ registerWindowUtil();
 
 // 앱에서 REST API 호출용 API 설정(전역에 저장된 : window.__MF_APP_CONFIG__) =============================
 const apiConfig = {
-	baseURL: import.meta.env.VITE_EXTERNAL_API_BASE_URL2,
+	baseURL: import.meta.env.VITE_API_BASE_URL,
 };
 initApiConfig(apiConfig);
 
